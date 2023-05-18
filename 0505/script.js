54ca9e4c0be6f8563129a0a6c5a9e157be4bbc09
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-05-18 11:54:46 +0800

    lottie

--- 0505/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let offset = window.pageYOffset;
-    let parallax = document.querySelector('.row::before');
-    parallax.style.transform = 'translateY(' + offset * 0.5 + 'px)';
-});
