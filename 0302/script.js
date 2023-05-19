b1e7fe0ded54cc72282c311b67d31a91dbf9fab3
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-05-20 00:36:18 +0800

    update 0302

--- 0302/script.js
+++ 0302/script.js
@@ -1,32 +1,32 @@
-$(document).ready(function () {
-    $('#red-button').click(function () {
+$(function () {
+    $('#red-button').on('click', function () {
         $('body').css('background-image', 'url(1.png)');
         $('#slider-text').css('color', '#b41a2f');
     });
 
-    $('#blue-button').click(function () {
+    $('#blue-button').on('click', function () {
         $('body').css('background-image', 'url(2.png)');
         $('#slider-text').css('color', '#333080');
     });
 
-    $('#green-button').click(function () {
+    $('#green-button').on('click', function () {
         $('body').css('background-image', 'url(3.png)');
         $('#slider-text').css('color', '#85ab41');
     });
 
-    $('#red-background').click(function () {
+    $('#red-background').on('click', function () {
         $(`body`).css('background-color', 'hsl(6, 70%, 90%)');
     });
 
-    $('#blue-background').click(function () {
+    $('#blue-background').on('click', function () {
         $(`body`).css('background-color', 'hsl(206, 70%, 90%)');
     });
 
-    $('#green-background').click(function () {
+    $('#green-background').on('click', function () {
         $('body').css('background-color', 'hsl(145, 70%, 90%)');
     });
 
-    $('#reset-button').click(function () {
+    $('#reset-button').on('click', function () {
         $('body').css('background-color', 'whitesmoke');
         $('body').css('background-image', 'none');
         $('#slider-text').css('color', '#333');
@@ -52,5 +52,3 @@ $(document).ready(function () {
     var timerId = setInterval(incrementNumber, 4000);
     setTimeout(function () { clearInterval(timerId); }, 24000);
 });
-
-
