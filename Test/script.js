61b4fdb1f1c0b8957d052e2a3be2a0cf41683c3c
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-05-27 01:19:01 +0800

    adds Test

---
+++ Test/script.js
@@ -0,0 +1,18 @@
+const fancyWord = document.querySelector('.fancy');
+const words = document.querySelectorAll('.word');
+
+fancyWord.addEventListener('mouseover', () => {
+    words.forEach((word) => {
+        if (!word.classList.contains('fancy')) {
+            word.style.opacity = '0';
+        }
+    });
+});
+
+fancyWord.addEventListener('mouseout', () => {
+    words.forEach((word) => {
+        if (!word.classList.contains('fancy')) {
+            word.style.opacity = '1';
+        }
+    });
+});
