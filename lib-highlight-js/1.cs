d8e4c239893682c00a3cbcd9c68fda059a2102ba
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-02 21:58:21 +0800

    google meeting

---
+++ lib-highlight-js/1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        float number1 = 3.14f;
+        float number2 = 2.718f;
+
+        float sum = number1 + number2;
+        float product = number1 * number2;
+
+        Console.WriteLine("Number 1: " + number1);
+        Console.WriteLine("Number 2: " + number2);
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Product: " + product);
+    }
+}
