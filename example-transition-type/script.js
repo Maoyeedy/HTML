b00302173e687cee369f805ef418c117b1ecbe26
Maoyeedy <100255436+Maoyeedy@users.noreply.github.com>
2023-06-05 07:09:08 +0800

    cleanup

---
+++ example-transition-type/script.js
@@ -0,0 +1,23 @@
+let canMoveCircles = true;
+document.addEventListener("keydown", function (event) {
+    if (event.code === "Space" && canMoveCircles) {
+        moveCircles();
+        canMoveCircles = false;
+        setTimeout(function () {
+            canMoveCircles = true;
+        }, 500);
+    }
+});
+
+function moveCircles() {
+    var circle1 = document.getElementById("circle1");
+    var circle2 = document.getElementById("circle2");
+
+    if (circle1.style.left === "80vw") {
+        circle1.style.left = "10vw";
+        circle2.style.left = "10vw";
+    } else {
+        circle1.style.left = "80vw";
+        circle2.style.left = "80vw";
+    }
+}
