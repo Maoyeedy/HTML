76484f286955bde54eb95a99e1dfe5d08c0a8d7d
Maoyeedy <maoyeedy@qq.com>
2023-10-15 00:19:05 +0800

    0.0

--- example-letter-fade/script.js
+++ example-letter-fade/script.js
@@ -1,18 +1,18 @@
-const fancyWord = document.querySelector('.fancy');
-const words = document.querySelectorAll('.word');
+const fancyWord = document.querySelector('.fancy')
+const words = document.querySelectorAll('.word')
 
 fancyWord.addEventListener('mouseover', () => {
     words.forEach((word) => {
         if (!word.classList.contains('fancy')) {
-            word.style.opacity = '0';
+            word.style.opacity = '1'
         }
-    });
-});
+    })
+})
 
 fancyWord.addEventListener('mouseout', () => {
     words.forEach((word) => {
         if (!word.classList.contains('fancy')) {
-            word.style.opacity = '1';
+            word.style.opacity = '0'
         }
-    });
-});
+    })
+})
