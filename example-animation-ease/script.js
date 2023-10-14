4ba414bedcd01e391f326c4e0f12973a7ead7545
Maoyeedy <maoyeedy@qq.com>
2023-10-15 00:07:50 +0800

    0.0

--- example-animation-ease/script.js
+++ example-animation-ease/script.js
@@ -1,23 +1,23 @@
-let canMoveCircles = true;
+let canMoveCircles = true
 document.addEventListener("keydown", function (event) {
     if (event.code === "Space" && canMoveCircles) {
-        moveCircles();
-        canMoveCircles = false;
+        moveCircles()
+        canMoveCircles = false
         setTimeout(function () {
-            canMoveCircles = true;
-        }, 500);
+            canMoveCircles = true
+        }, 500)
     }
-});
+})
 
-function moveCircles() {
-    var circle1 = document.getElementById("circle1");
-    var circle2 = document.getElementById("circle2");
+function moveCircles () {
+    var circle1 = document.getElementById("circle1")
+    var circle2 = document.getElementById("circle2")
 
     if (circle1.style.left === "80vw") {
-        circle1.style.left = "10vw";
-        circle2.style.left = "10vw";
+        circle1.style.left = "10vw"
+        circle2.style.left = "10vw"
     } else {
-        circle1.style.left = "80vw";
-        circle2.style.left = "80vw";
+        circle1.style.left = "80vw"
+        circle2.style.left = "80vw"
     }
 }
--- example-language-switch/script.js
+++ example-language-switch/script.js
@@ -1,17 +1,17 @@
-var cnBtn = document.getElementById("cnBtn");
-var enBtn = document.getElementById("enBtn");
-var content = document.getElementById("content");
+let cnBtn = document.getElementById("cnBtn")
+let enBtn = document.getElementById("enBtn")
+let content = document.getElementById("content")
 
 cnBtn.addEventListener("click", function () {
-    content.querySelector(".cn-text").style.display = "block";
-    content.querySelector(".en-text").style.display = "none";
-    cnBtn.classList.add("active");
-    enBtn.classList.remove("active");
-});
+    content.querySelector(".cn-text").style.display = "block"
+    content.querySelector(".en-text").style.display = "none"
+    cnBtn.classList.add("active")
+    enBtn.classList.remove("active")
+})
 
 enBtn.addEventListener("click", function () {
-    content.querySelector(".cn-text").style.display = "none";
-    content.querySelector(".en-text").style.display = "block";
-    enBtn.classList.add("active");
-    cnBtn.classList.remove("active");
-});
+    content.querySelector(".cn-text").style.display = "none"
+    content.querySelector(".en-text").style.display = "block"
+    enBtn.classList.add("active")
+    cnBtn.classList.remove("active")
+})
